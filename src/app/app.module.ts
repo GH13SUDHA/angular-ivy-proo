695fc3153537a84933bb3f98c3730451d2e56da3
GH13SUDHA <srisudha6420@gmail.com>
2022-02-16 20:12:24 +0530

    5 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,
+  RouterModule.forRoot([
+    {path:'', component:ProductListComponent}
+  ]) 
+],
+  declarations: [ AppComponent, TopBarComponent, ProductListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
